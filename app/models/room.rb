fe039a47aac9ea7403b37404dfbdcf4a6a5bd619
nejdetkadir <nejdetkadir.550@gmail.com>
2021-05-05 19:44:17 +0300

    Show participants on show page of room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,16 @@
 class Room < ApplicationRecord
   belongs_to :user
 
-  has_many :participants
+  has_many :participants, dependent: :destroy
 
   mount_uploader :avatar, RoomAvatarUploader
+
+  after_create :add_me_as_participant
+
+  def add_me_as_participant
+    p = Participant.new
+    p.room = self
+    p.user = self.user
+    p.save
+  end
 end
