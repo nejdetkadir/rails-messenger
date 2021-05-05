4836d69a34fefbdb9f55a46c233adf21bd52827c
nejdetkadir <nejdetkadir.550@gmail.com>
2021-05-05 20:26:34 +0300

    Add has_friendship callbacks to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,16 @@ class User < ApplicationRecord
     end
     user
   end
+
+  def on_friendship_created(friendship)
+  end
+  
+  def on_friendship_accepted(friendship)
+  end
+  
+  def on_friendship_blocked(friendship)
+  end
+  
+  def on_friendship_destroyed(friendship)
+  end
 end
